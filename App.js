1adad0bd18411bc165c273ee4690a20318745dfa
Aditya Agrawal <agraw17107@gapps.uwcsea.edu.sg>
2021-09-17 21:09:08 +0800

    added first launch logic

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [firstLaunch, setFirstLaunch ] = useState(null);
+  useEffect(() => {
+	const asyncCheck = async () => { 	
+		await AsyncStorage.getItem("firstLaunch").then(value => { setFirstLaunch(value == null) })
+  	}
+	asyncCheck()
+  })
+  
+  if (firstLaunch == null){
+	  return (
+	    <View style={styles.container}>
+	      <Text>Loading...</Text>
+	      <StatusBar style="auto" />
+	    </View>
+	  )   
+  } else {
+	//TO-DO Call Once
+	if (firstLaunch) {
+	  //AsyncStorage.setItem("firstLaunch", false)
+	  return (
+	    <View style={styles.container}>
+	      <Text>First Launch!</Text>
+	      <StatusBar style="auto" />
+	    </View>
+	  )
+	} else {
+	  return (
+	    <View style={styles.container}>
+	      <Text>Welcome Back</Text>
+	      <StatusBar style="auto" />
+	    </View>
+	  )
+	}
+  }
 }
 
 const styles = StyleSheet.create({
