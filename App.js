94b337e4dd833268b1f53f6c5eec6f04c910c822
Aditya Agrawal <agraw17107@gapps.uwcsea.edu.sg>
2021-09-17 21:57:40 +0800

    working first launch test

--- App.js
+++ App.js
@@ -5,39 +5,36 @@ import { StyleSheet, Text, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
-  const [firstLaunch, setFirstLaunch ] = useState(null);
+  const [firstLaunch, setFirstLaunch ] = useState(false);
   useEffect(() => {
-	const asyncCheck = async () => { 	
-		await AsyncStorage.getItem("firstLaunch").then(value => { setFirstLaunch(value == null) })
-  	}
-	asyncCheck()
-  })
-  
-  if (firstLaunch == null){
-	  return (
-	    <View style={styles.container}>
-	      <Text>Loading...</Text>
-	      <StatusBar style="auto" />
-	    </View>
-	  )   
-  } else {
-	//TO-DO Call Once
-	if (firstLaunch) {
-	  //AsyncStorage.setItem("firstLaunch", false)
-	  return (
-	    <View style={styles.container}>
-	      <Text>First Launch!</Text>
-	      <StatusBar style="auto" />
-	    </View>
-	  )
-	} else {
-	  return (
-	    <View style={styles.container}>
-	      <Text>Welcome Back</Text>
-	      <StatusBar style="auto" />
-	    </View>
-	  )
+	console.log("._.")
+	const asyncCheck = async () => { 
+		await AsyncStorage.getItem("firstLaunch").then((value) => { 
+			if (value == null) {
+				setFirstLaunch(true)
+	  			AsyncStorage.setItem("firstLaunch", false)
+			} else {
+				setFirstLaunch(false)
+			}
+		})
 	}
+	asyncCheck()
+  }, [])
+  
+  if (firstLaunch) {
+    return (
+      <View style={styles.container}>
+        <Text>First Launch!</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome Back</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
   }
 }
 
